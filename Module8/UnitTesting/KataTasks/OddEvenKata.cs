0da5b052d5c4e2b4ce7457ef2948ce7f94ad467c
Olga Shtykova <olgashtykova@yahoo.com>
2021-05-30 23:07:56 +0300

    Add test ShouldPrintOddEvenAndPrimeNumbers. Add method PrintNumbers. Add private methods to check if a number is even, odd or prime.

--- Module8/UnitTesting/KataTasks/OddEvenKata.cs
+++ Module8/UnitTesting/KataTasks/OddEvenKata.cs
@@ -1,10 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace KataTasks
 {
     public class OddEvenKata
     {
+        private const int From = 1;
+        private const int To = 100;
+
+        public string PrintNumbers()
+        {
+            var result = string.Empty;
+            var newNumber = string.Empty;
+
+            for (int number = From; number <= To; number++)
+            {
+                if (IsPrime(number))
+                {
+                    newNumber = Convert.ToString(number);
+                }
+                else if (IsEven(number))
+                {
+                    newNumber = "Even";
+                }
+                else if (IsOdd(number))
+                {
+                    newNumber = "Odd";
+                }
+                
+                result = string.Concat(result, " ", newNumber);
+            }
+
+            return result.Trim();
+        }
+        
+        private static bool IsEven(int number)
+        {
+            if (number >= 2 && number % 2 == 0)
+                return true;
+            return false;
+        }
+
+        private static bool IsOdd(int number)
+        {
+            if (number % 2 != 0)
+                return true;
+            return false;
+        }
+
+        private static bool IsPrime(int number)
+        {
+            if (number < 2) return false;
+            if (IsEven(number)) return false;
+
+            for (int i = 2; i <= number / 2; i++)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
     }
 }
---
+++ Module8/UnitTesting/KataTasksTests/OddEvenKataTests.cs
@@ -0,0 +1,24 @@
+﻿using KataTasks;
+using NUnit.Framework;
+
+namespace KataTasksTests
+{
+    public class OddEvenKataTests
+    {
+        private OddEvenKata _oddEvenKata;
+
+        [SetUp]
+        public void Setup()
+        {
+            _oddEvenKata = new OddEvenKata();
+        }
+
+        [Test]
+        public void ShouldPrintOddEvenAndPrimeNumbers()
+        {
+            var result = _oddEvenKata.PrintNumbers(); 
+
+            Assert.NotNull(result, $"{result}");
+        }
+    }
+}
