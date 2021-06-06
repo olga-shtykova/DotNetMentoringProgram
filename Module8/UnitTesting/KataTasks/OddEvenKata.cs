75d1d1acf6724099ba08ed7b0b9985ae0e84d0c9
Olga Shtykova <olgashtykova@yahoo.com>
2021-06-06 15:34:10 +0300

    Refactor OddEvenKata
    Split internal logic within PrintNumbers method.
    Add tests to check parameters iin the range.

--- Module8/UnitTesting/KataTasks/OddEvenKata.cs
+++ Module8/UnitTesting/KataTasks/OddEvenKata.cs
@@ -2,37 +2,49 @@
 
 namespace KataTasks
 {
-    public class OddEvenKata
+    public static class OddEvenKata
     {
-        public string PrintNumbers(int from, int to)
+        public static string PrintNumbers(int from, int to)
         {
             if (from <= 0 || to <= 0 || from >= to)
                 throw new ArgumentException("Value does not fall within the expected range.");
 
+            return CheckOddEvenPrimeOfNumbersInRange(from, to);
+        }
+
+        private static string CheckOddEvenPrimeOfNumbersInRange(int from, int to)
+        {
             var result = string.Empty;
-            var newNumber = string.Empty;
 
             for (int number = from; number <= to; number++)
             {
-                if (IsPrime(number))
-                {
-                    newNumber = Convert.ToString(number);
-                }
-                else if (IsEven(number))
-                {
-                    newNumber = "Even";
-                }
-                else if (IsOdd(number))
-                {
-                    newNumber = "Odd";
-                }
-                
+                var newNumber = CheckOddEvenPrimeOfaSingleNumber(number);
                 result = string.Concat(result, " ", newNumber);
             }
 
             return result.Trim();
         }
-        
+
+        private static string CheckOddEvenPrimeOfaSingleNumber(int number)
+        {
+            var result = string.Empty;
+
+            if (IsPrime(number))
+            {
+                result = Convert.ToString(number);
+            }
+            else if (IsEven(number))
+            {
+                result = "Even";
+            }
+            else if (IsOdd(number))
+            {
+                result = "Odd";
+            }
+
+            return result;
+        }
+
         private static bool IsEven(int number)
         {
             return number >= 2 && number % 2 == 0;
--- Module8/UnitTesting/KataTasksTests/OddEvenKataTests.cs
+++ Module8/UnitTesting/KataTasksTests/OddEvenKataTests.cs
@@ -5,42 +5,55 @@ namespace KataTasksTests
 {
     public class OddEvenKataTests
     {
-        private OddEvenKata _oddEvenKata;
-
-        [SetUp]
-        public void Setup()
-        {
-            _oddEvenKata = new OddEvenKata();
-        }
-
-        [TestCase(1, 100)]
-        [Test]
-        public void PrintOddEvenAndPrimeNumbers_ResultShouldNotBeNull(int num1, int num2)
-        {
-            // Arrange/Act
-            var result = _oddEvenKata.PrintNumbers(num1, num2);
-
-            // Assert
-            Assert.NotNull(result, $"{result}");
-        }
-
         [TestCase(1, 5, "Odd Even 3 Even 5")]
         [Test]
-        public void ShouldPrintOddEvenAndPrimeNumbers(int num1, int num2, string expectedResult)
+        public void IfAllDataIsValid_ShouldPrintOddEvenAndPrimeNumbers(int num1, int num2, string expectedResult)
         {
             // Arrange/Act
-            var result = _oddEvenKata.PrintNumbers(num1, num2);
+            var result = OddEvenKata.PrintNumbers(num1, num2);
 
             // Assert
             Assert.That(expectedResult, Is.EqualTo(result));
         }
-
-        [TestCase(0, -10)]
+        
+        [TestCase(-1, 10)]
         [Test]
-        public void IfRangeIsInvalid_ThrowArgumentException(int num1, int num2)
+        public void IfFirstParameterIsNegative_ThrowArgumentException(int num1, int num2)
         {
             // Assert
-            Assert.That(() => _oddEvenKata.PrintNumbers(num1, num2), Throws.Exception);
+            Assert.That(() => OddEvenKata.PrintNumbers(num1, num2), Throws.Exception);
+        }
+
+        [TestCase(1, -10)]
+        [Test]
+        public void IfSecondParameterIsNegative_ThrowArgumentException(int num1, int num2)
+        {
+            // Assert
+            Assert.That(() => OddEvenKata.PrintNumbers(num1, num2), Throws.Exception);
+        }
+
+        [TestCase(5, 1)]
+        [Test]
+        public void IfSecondParameterIsLessThanTheFirst_ThrowArgumentException(int num1, int num2)
+        {
+            // Assert
+            Assert.That(() => OddEvenKata.PrintNumbers(num1, num2), Throws.Exception);
+        }
+
+        [TestCase(0, 10)]
+        [Test]
+        public void IfFirstParameterIsZero_ThrowArgumentException(int num1, int num2)
+        {
+            // Assert
+            Assert.That(() => OddEvenKata.PrintNumbers(num1, num2), Throws.Exception);
+        }
+
+        [TestCase(5, 0)]
+        [Test]
+        public void IfSecondParameterIsZero_ThrowArgumentException(int num1, int num2)
+        {
+            // Assert
+            Assert.That(() => OddEvenKata.PrintNumbers(num1, num2), Throws.Exception);
         }
     }
 }
